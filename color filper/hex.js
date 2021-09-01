335f672a04b25a7643eb4fe5f41a7bf67724e8d0
AbidHussainDar <46255070+AbidHussainDar@users.noreply.github.com>
2021-09-01 22:31:45 +0530

    Add files via upload

---
+++ color filper/hex.js
@@ -0,0 +1,19 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+const heading = document.querySelector('h2')
+
+btn.addEventListener('click', () => {
+  let hexColor = '#'
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()]
+  }
+
+  color.textContent = hexColor
+  document.body.style.backgroundColor = hexColor
+  heading.style.color = hexColor
+})
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length)
+}
